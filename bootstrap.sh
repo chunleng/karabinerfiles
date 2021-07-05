675991342d8a448c65e37748c9d9401b62ef23e9
Lim Chun Leng <lim@logly.co.jp>
2021-07-05 19:59:14 +0900

    Change to copy as karabiner overrides the symlink

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,4 +5,4 @@ set eu
 APP_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )"
 
 mkdir -p ${HOME}/.config/karabiner
-ln -sfn ${APP_PATH}/karabiner.json ${HOME}/.config/karabiner/karabiner.json
+cp ${APP_PATH}/karabiner.json ${HOME}/.config/karabiner/karabiner.json
